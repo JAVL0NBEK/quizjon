f662b3e5c3582972ee55c2e199edc4ffb8090c64
j_sobirov <J.Sobirov@uzinfocom.uz>
2025-05-20 11:24:55 +0500

    commit

--- src/main/java/com/smart/quiz/dto/QuizState.java
+++ src/main/java/com/smart/quiz/dto/QuizState.java
@@ -3,8 +3,13 @@ package com.smart.quiz.dto;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 public class QuizState {
+  // Getter va Setter lar
   private Long subjectId; // Tanlangan fan IDsi
   private Map<String, List<Long>> sections; // Dinamik bo‘limlar
   private String currentSection; // Joriy bo‘lim
@@ -21,23 +26,10 @@ public class QuizState {
     this.sections = new HashMap<>();
   }
 
-  // Getter va Setter lar
-  public Long getSubjectId() { return subjectId; }
-  public void setSubjectId(Long subjectId) { this.subjectId = subjectId; }
-  public Map<String, List<Long>> getSections() { return sections; }
-  public void setSections(Map<String, List<Long>> sections) { this.sections = sections; }
-  public String getCurrentSection() { return currentSection; }
-  public void setCurrentSection(String currentSection) { this.currentSection = currentSection; }
-  public int getCurrentQuestionIndex() { return currentQuestionIndex; }
-  public void setCurrentQuestionIndex(int index) { this.currentQuestionIndex = index; }
   public void incrementQuestionIndex() { this.currentQuestionIndex++; }
-  public int getCorrectAnswersCount() { return correctAnswersCount; }
-  public void setCorrectAnswersCount(int count) { this.correctAnswersCount = count; }
+
   public void incrementCorrectAnswers() { this.correctAnswersCount++; }
-  public int getWrongAnswersCount() { return wrongAnswersCount; }
-  public void setWrongAnswersCount(int count) { this.wrongAnswersCount = count; }
+
   public void incrementWrongAnswers() { this.wrongAnswersCount++; }
-  public boolean isActive() { return active; }
-  public void setActive(boolean active) { this.active = active; }
 
 }
